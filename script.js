b94d1ed7ef2697259d4e33aa4df0db9c4709f50f
VERHILLE Arnaud <gist974@gmail.com>
2025-03-06 21:59:51 +0400

    Unused files

--- script.js
+++
@@ -1,5 +0,0 @@
-function displayPersona(personaName) {
-    const personaImg = document.getElementById('persona-image');
-    personaImg.src = `personas/${personaName}.png`;
-    personaImg.style.width = '150%';  // augmente la taille de 1,5 fois
-} 
